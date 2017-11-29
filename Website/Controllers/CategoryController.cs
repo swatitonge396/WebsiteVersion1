f70cb5dec467ce11a733046a4888854185113d40
swatitonge <swatitonge@yahoo.co.in>
2017-11-29 15:52:53 -0500

    changes to category

--- Website/Controllers/CategoryController.cs
+++ Website/Controllers/CategoryController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,9 +12,21 @@ namespace Website.Controllers
     public class CategoryController : Controller
     {
         // GET: Category
-        
+
         public ActionResult Index()
         {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(int Category)
+        {   //changes the category of user
+            MyDbContext db = new MyDbContext();
+            string CurrentUser = User.Identity.GetUserId();
+            var manager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new MyDbContext()));
+            ApplicationUser user = manager.FindById(CurrentUser);
+            user.Category = Category;
+            manager.UpdateAsync(user);
+            ViewBag.msg = "New Category Selected " + db.Category.Find(Category)?.Name;
             return View();
         }
     }
