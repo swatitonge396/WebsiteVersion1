bb4a80ae73659b5cfa1fa68cb08d2bd9f5e15ef7
yuvaranick <yuvarani.kumaresan@gmail.com>
2017-12-04 01:41:26 -0500

    Fixed Bug in the Category Selection . Fixed views in whole website with template . Added Change Password Functionality .

--- Website/Controllers/AccountController.cs
+++ Website/Controllers/AccountController.cs
@@ -206,15 +206,16 @@ namespace Website.Controllers
                 if (user == null || !(await UserManager.IsEmailConfirmedAsync(user.Id)))
                 {
                     // Don't reveal that the user does not exist or is not confirmed
-                    return View("ForgotPasswordConfirmation");
-                }
+                    //return View("ForgotPasswordConfirmation");
 
-                // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
-                // Send an email with this link
-                // string code = await UserManager.GeneratePasswordResetTokenAsync(user.Id);
-                // var callbackUrl = Url.Action("ResetPassword", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);		
-                // await UserManager.SendEmailAsync(user.Id, "Reset Password", "Please reset your password by clicking <a href=\"" + callbackUrl + "\">here</a>");
-                // return RedirectToAction("ForgotPasswordConfirmation", "Account");
+
+                    // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
+                    // Send an email with this link
+                     string code = await UserManager.GeneratePasswordResetTokenAsync(user.Id);
+                     var callbackUrl = Url.Action("ResetPassword", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);		
+                     await UserManager.SendEmailAsync(user.Id, "Reset Password", "Please reset your password by clicking <a href=\"" + callbackUrl + "\">here</a>");
+                     return RedirectToAction("ForgotPasswordConfirmation", "Account");
+                }
             }
 
             // If we got this far, something failed, redisplay form
--- Website/Controllers/CategoryController.cs
+++ Website/Controllers/CategoryController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,22 +12,39 @@ namespace Website.Controllers
 {
     public class CategoryController : Controller
     {
+        private MyDbContext db = new MyDbContext();
+
         // GET: Category
 
         public ActionResult Index()
         {
-            return View();
+            var manager = new UserManager<ApplicationUser>(
+                 new UserStore<ApplicationUser>(
+                     new MyDbContext()));
+            int UserCategory = manager.FindById(User.Identity.GetUserId()).Category;
+            var list = (from cat in db.Category select cat).ToList();
+            foreach (Category cat in list)
+            {
+                if (cat.Id == UserCategory)
+                {
+                    cat.Selected = true;
+                }
+            }
+            return View(list);
         }
+
         [HttpPost]
-        public ActionResult Index(int Category)
+        public async System.Threading.Tasks.Task<ActionResult> Index(int Category)
         {   //changes the category of user
             MyDbContext db = new MyDbContext();
             string CurrentUser = User.Identity.GetUserId();
             var manager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new MyDbContext()));
             ApplicationUser user = manager.FindById(CurrentUser);
             user.Category = Category;
-            manager.UpdateAsync(user);
-                       return RedirectToAction("Index", "Product");
+            var result = await manager.UpdateAsync(user);
+            return RedirectToAction("Index", "Product");
         }
+
+
     }
 }
